fc9c5a066c8df3f7ead71af2b332884342f2f502
qi_xinghua <qi_xinghua@hithinksoft.com>
2018-09-03 16:30:51 +0800

    修改index.html位置（webpack.dev.config.js, webpack.prop.config.js）； 添加热部署webpack.dev.config.js中设置hot:true, 添加new webpack.HotModuleReplacementPlugin()， package.json中添加"dev": "webpack-dev-server --hot --inline --progress --config build/webpack.dev.conf.js",

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -48,9 +48,9 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: utils.assetsPath('css/[name].[contenthash].css'),
       // Setting the following option to `false` will not extract CSS from codesplit chunks.
       // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`, 
+      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
       // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
-      allChunks: true,
+      allChunks: true
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
@@ -66,7 +66,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: process.env.NODE_ENV === 'testing'
         ? 'index.html'
         : config.build.index,
-      template: 'index.html',
+      template: 'src/index.html',    //修改入口文件地址
       inject: true,
       minify: {
         removeComments: true,
--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/',    //打包后文件路径
     proxyTable: {},
 
     // Various Dev Server settings
